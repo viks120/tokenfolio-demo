ab18ff423635cd21f3f2f7f3bfff9ece605d2c6a
Vikrant <viks120@users.noreply.github.com>
2025-04-15 04:04:28 -0400

    Build error fix

--- app/api/crypto/[currency]/route.tsx
+++ app/api/crypto/[currency]/route.tsx
@@ -3,15 +3,9 @@ import crypto_gbp from '../cryp_obj_gbp.json'
 import crypto_inr from '../cryp_obj_inr.json'
 import crypto_eur from '../cryp_obj_eur.json'
 
-export async function GET(request: Request, context: { params: { currency: string }}) {
-
-  const params = await context.params;
-
+export async function GET(request: Request, {params}: { params: { currency: string }}) {
   let cryptos;
-
   const curr = params.currency.toLowerCase();
-
-  console.log(params)
 
     if(curr == 'usd') {
       cryptos = crypto_usd;
